b0cbb2c85fadce7d91b83116b84d408501485326
Eli Clemente Gordillo Foster <elifosterwy@gmail.com>
2023-11-15 12:15:10 -0800

    :package: :octocat: Item models now generated using datagen Remove datagen cache files from vcs

--- src/main/java/io/github/elifoster/santastoys/datagen/SantasToysItemModelsProvider.java
+++ src/main/java/io/github/elifoster/santastoys/datagen/SantasToysItemModelsProvider.java
@@ -3,8 +3,15 @@ package io.github.elifoster.santastoys.datagen;
 import io.github.elifoster.santastoys.blocks.BlockHandler;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.client.model.generators.ItemModelBuilder;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.ForgeRegistries;
+
+import static io.github.elifoster.santastoys.blocks.BlockHandler.*;
+import static io.github.elifoster.santastoys.items.ItemHandler.*;
 
 public class SantasToysItemModelsProvider extends ItemModelProvider {
     public SantasToysItemModelsProvider(PackOutput output, String modid, ExistingFileHelper existingFileHelper) {
@@ -13,11 +20,22 @@ public class SantasToysItemModelsProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        withExistingParent(BlockHandler.SPICED_SAND.getId().getPath(), loc(BlockHandler.NAME_SPICED_SAND));
-        withExistingParent(BlockHandler.HEAVY_LIGHT.getId().getPath(), mcLoc("glowstone"));
+        withExistingParent(SPICED_SAND.getId().getPath(), modLoc("block/" + BlockHandler.NAME_SPICED_SAND));
+        withExistingParent(HEAVY_LIGHT.getId().getPath(), mcLoc("glowstone"));
+        basicItem(MATCH.get());
+        basicItem(ENDER_BLASTER.get());
+        basicItem(NETHER_BLASTER.get());
     }
 
-    private ResourceLocation loc(String name) {
-        return modLoc("block/" + name);
+    /**
+     * Helper method for generating a simple minecraft:item/handheld model json. For example: minecraft:iron_axe.
+     * @param item the item
+     * @return
+     */
+    private ItemModelBuilder handheldItem(Item item) {
+        ResourceLocation loc = ForgeRegistries.ITEMS.getKey(item);
+        return getBuilder(loc.toString())
+          .parent(new ModelFile.UncheckedModelFile("item/handheld"))
+          .texture("layer0", new ResourceLocation(loc.getNamespace(), "item/" + loc.getPath()));
     }
 }
